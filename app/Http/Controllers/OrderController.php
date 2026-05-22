5da2333cc15bf4c28000594976f5e4723c8fbaa5
Polina Seregina <p.seregina2015@yandex.ru>
2026-05-22 12:59:37 +0300

    draft of the order model

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * список продаж Юзера
+     */
+
+    /**
+     * список покупок пользователя
+     */
+
+    /**
+     * история ордеров пользователя (все вместе)
+     */
+}
