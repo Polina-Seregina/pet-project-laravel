44f1bcf4ff938ac38c62ad644fcaba28bcbe1fcd
Polina Seregina <p.seregina2015@yandex.ru>
2026-08-22 00:04:26 +0300

    wallet and transaction views changed

--- resources/views/transaction/history.blade.php
+++ resources/views/transaction/history.blade.php
@@ -6,52 +6,56 @@
 
 <section class="theme-banner-one">
     <div class="title-one text-center mb-70">
-        <h3 class="main-title z-2">
-            TRANSACTION HISTORY
+        <h3 style="font-size: 50px; letter-spacing: 15px; color: #93837d33" class="main-title z-2">
+            ИСТОРИЯ ОПЕРАЦИЙ
         </h3>
     </div>
 </section>
 
-<div>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-            <section class="ht-project-section pt-140 pb-140 pt-lg-60 pb-lg-60">
-                <blockquote style="text-align: center;" class="mt-10 mb-30 ml-10">
-                    @if (count($transactions) !== 0)
-                    <table class="mb-6" style="width: 100%; border-collapse: collapse; rgb(0, 0, 0);">
-                        <thead>
-                            <tr>
-                                <th style="text-align: center; border: 1px solid"> <p style="font-size: 30px"> Transaction type </p> </th>
-                                <th style="text-align: center; border: 1px solid"> <p style="font-size: 30px"> Amount </p> </th>
-                                <th style="text-align: center; border: 1px solid"> <p style="font-size: 30px"> Date </p> </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($transactions as $transaction)
-                            <tr>
-                                <td style="text-align: center; border: 1px solid"> {{ $transaction->type->label() }} </td>
-                                <td style="text-align: center; border: 1px solid"> {{ $transaction->amount }} </td>
-                                <td style="text-align: center; border: 1px solid"> {{ $transaction->created_at }} </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    
-                    {{ $transactions->links() }}
-                    @else
-                    <div style="text-align: center;" class="footer-widget mb-10">
-						<p class="show-product-count text-heding fw-medium" style="text-align: center; font-size: 20px;"> Транзакций пока не было</p>
-                    </div>
-                    <div style="text-align: center;" class="footer-widget mb-30">
-                        <button class="ht-btn bs-style mb-2" type="submit">
-                            <a href="{{ route('wallet.replenishment.form') }}"> Top up your balance </a>
-                        </button>
-					</div>
-                    @endif
-                </blockquote>
-            </section>
-        </div>    
-    </div>
+
+<div class="max-w-7xl mx-auto lg:px-8 space-y-6 ">
+    <div class="p-4 bg-white shadow sm:rounded-lg  product-wrapper">
+        @if (count($transactions) !== 0)
+        <table class="mb-6" style="width: 100%; border-collapse: collapse; rgb(0, 0, 0);">
+            <thead>
+                <tr>
+                    <th style="text-align: center; border: 1px solid"> <p style="font-size: 30px"> Тип операции </p> </th>
+                    <th style="text-align: center; border: 1px solid"> <p style="font-size: 30px"> Сумма </p> </th>
+                    <th style="text-align: center; border: 1px solid"> <p style="font-size: 30px"> Дата и время </p> </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($transactions as $transaction)
+                <tr>
+                    <td style="text-align: center; border: 1px solid"> {{ $transaction->type->label() }} </td>
+                    <td style="text-align: center; border: 1px solid"> {{ $transaction->amount }} </td>
+                    <td style="text-align: center; border: 1px solid"> {{ $transaction->created_at }} </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        
+        {{ $transactions->links() }}
+        @else
+        <div style="text-align: center;" class="mb-10">
+            <p class="product-title mt-9 mb-0" style="text-align: center; font-size: 20px;"> Транзакций пока не было </p>
+        </div>
+
+        <div style="text-align: center;" class="footer-widget mb-30">
+            <button class="ht-btn bs-style mb-2" type="submit">
+                <a href="{{ route('wallet.replenishment.form') }}"> Пополнить баланс </a>
+            </button>
+        </div>
+        @endif
+    </div>    
+</div>
+
+<div style="height: 30px;"></div>
+
+<div class="title-one text-center mt-20">
+    <p class="text-sm text-gray-700 leading-5 dark:text-gray-400">
+        <a href="javascript:history.back()"> <- Назад </a>
+    </p>
 </div>
 
 @include('partials.footer')
